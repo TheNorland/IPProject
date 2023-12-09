ae6130bcae5d3cadcc4e79de87a2f86f4055f51f
TheNorland <39226781+TheNorland@users.noreply.github.com>
2023-12-09 01:34:02 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,22 @@
 // api/index.js
-module.exports = (req, res) => {
-  res.status(200).send('Hello, this is your serverless function!');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async (req, res) => {
+  try {
+    // Get the IP address from the request headers
+    const ipAddress = req.headers['x-real-ip'] || req.connection.remoteAddress;
+
+    // Display a "Thanks!" message
+    res.status(200).send('Thanks!');
+
+    // Log the IP address to a file
+    const logFilePath = path.join(__dirname, 'ip_logs.txt');
+    const logEntry = `${new Date().toISOString()} - ${ipAddress}\n`;
+
+    fs.appendFileSync(logFilePath, logEntry, 'utf-8');
+  } catch (error) {
+    console.error('Error processing request:', error);
+    res.status(500).send('Internal Server Error');
+  }
 };
