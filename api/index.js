039498550185d07339fe5bdbe1b27aa2707af536
TheNorland <39226781+TheNorland@users.noreply.github.com>
2023-12-09 23:48:02 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,19 +4,12 @@ const path = require('path');
 
 module.exports = async (req, res) => {
   try {
-    // Get the IP address from the request headers
-    const ipAddress = req.headers['x-real-ip'] || req.connection.remoteAddress;
-
-    // Display a "Thanks!" message
-    res.status(200).send('Thanks!');
-
-    // Log the IP address to a file
     const logFilePath = path.join(__dirname, 'ip_logs.txt');
-    const logEntry = `${new Date().toISOString()} - ${ipAddress}\n`;
-
-    fs.appendFileSync(logFilePath, logEntry, 'utf-8');
+    const data = fs.readFileSync(logFilePath, 'utf-8');
+    const ipAddresses = data.split('\n').filter(Boolean);
+    res.json({ ipAddresses });
   } catch (error) {
-    console.error('Error processing request:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('Error reading IP logs:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
